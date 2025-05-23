c680fcf0c804a560949634d2b16e7aa7c5af8310
Iago V. Rocha <iagovieira3562@gmail.com>
2025-05-23 01:48:39 -0300

    Teste manual de instaciar classes na main

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-import Funcionario
+from Funcionario import Funcionario
+from Sistema import Sistema
 
-f = Funcionario()
+if __name__ == "__main__":
+    sistema = Sistema()
+    funcionario1 = Funcionario(sistema, "NOME FUNCIONARIO", "XX.XXX.XXX-X", "XXX.XXX.XXX-XX", 1993, 11, 11, "Masculino", 1, "Administrativo", "Funcionario Administrativo", 1)
+    sistema.cadastroFuncionario(funcionario1)
+    print(sistema.cadastroFuncionario(funcionario1))
+    print(sistema.cadastroFuncionarios)
+    sistema.cadastroFuncionarios[1].Exibir()
