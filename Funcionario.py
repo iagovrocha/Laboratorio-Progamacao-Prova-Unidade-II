80d460cf7814cbbffb927f6c8ec334f0417e50f9
Iago V. Rocha <iagovieira3562@gmail.com>
2025-05-22 21:52:05 -0300

    Adicionado metodo de cadastro na classe Funcionario

--- Funcionario.py
+++ Funcionario.py
@@ -1,10 +1,14 @@
-import Pessoa
-
+from __future__ import annotations
+from Pessoa import Pessoa
 class Funcionario(Pessoa):
-    def __init__(self, nome: str, rg: str, cpf: str, anoNasc: int, mesNasc: int,
+    def __init__(self, sistema: "Sistema", nome: str, rg: str, cpf: str, anoNasc: int, mesNasc: int,
                   diaNasc: int, sexo: str, matricula: int, setor: str, cargo: str, nivel: int):
         Pessoa.__init__(self, nome, rg, cpf, anoNasc, mesNasc, diaNasc, sexo)
+        self.sistema = sistema
         self.__matricula = matricula
         self.__setor = setor
         self.__cargo = cargo
         self.__nivel = nivel
+
+    def cadastrarFuncionario(self):
+        self.sistema.cadastroFuncionario(self)
