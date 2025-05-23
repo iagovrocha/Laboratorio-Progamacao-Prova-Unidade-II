e4a64c7b330608614d581803b870e7995e251ba6
Iago V. Rocha <iagovieira3562@gmail.com>
2025-05-23 17:06:54 -0300

    Update main.py para testes

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from __future__ import annotations
 from Funcionario import Funcionario
 from Sistema import Sistema
+from Salario import *
 
 if __name__ == "__main__":
     sistema = Sistema()
     funcionario1 = Funcionario(sistema, "NOME FUNCIONARIO", "XX.XXX.XXX-X", "XXX.XXX.XXX-XX", 1993, 11, 11, "Masculino", 1, "Administrativo", "Funcionario Administrativo", 1)
-    sistema.cadastroFuncionario(funcionario1)
+    # print(sistema.cadastroFuncionario(funcionario1))
     print(sistema.cadastroFuncionario(funcionario1))
-    print(sistema.cadastroFuncionarios)
-    sistema.cadastroFuncionarios[1].Exibir()
+    print(sistema.getCadastroFuncionarios())
+    sistema.getCadastroFuncionarios()[1].Exibir()
+
+    salario = Salario(True, strategy = ProfessorNivelI)
+    print(salario.calcularSalario())
