2df2c4dbb9e4653badbfe60cf1d2ff86dab4ca93
Iago V. Rocha <iagovieira3562@gmail.com>
2025-05-22 23:09:57 -0300

    Adcionado Exibir() em Funcionario com uso do polimorfismo

--- Funcionario.py
+++ Funcionario.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from Pessoa import Pessoa
 class Funcionario(Pessoa):
-    def __init__(self, sistema: "Sistema", nome: str, rg: str, cpf: str, anoNasc: int, mesNasc: int,
+    def __init__(self, sistema: "Sistema", nome: str, rg: str, cpf: str, anoNasc: int, mesNasc: int, # type: ignore
                   diaNasc: int, sexo: str, matricula: int, setor: str, cargo: str, nivel: int):
         Pessoa.__init__(self, nome, rg, cpf, anoNasc, mesNasc, diaNasc, sexo)
         self.sistema = sistema
@@ -10,5 +10,23 @@ class Funcionario(Pessoa):
         self.__cargo = cargo
         self.__nivel = nivel
 
-    def cadastrarFuncionario(self):
-        self.sistema.cadastroFuncionario(self)
+    # def cadastrarFuncionario(self):
+    #     self.sistema.cadastroFuncionario(self)
+    def getMatricula(self) -> str:
+        return self.__matricula
+    def getSetor(self) -> str:
+        return self.__setor
+    def getCargo(self) -> str:
+        return self.__cargo
+    def getNivel(self) -> int:
+        return self.__nivel
+
+    def Exibir(self):
+        Pessoa.Exibir(self)
+        print(f"{' Informações Funcionario ':-^50}")
+        print(f"""
+            Matricula: {self.getMatricula()}
+            Setor: {self.getSetor()}
+            Cargo: {self.getCargo()}
+            Nivel: {self.getNivel()}
+            """)
