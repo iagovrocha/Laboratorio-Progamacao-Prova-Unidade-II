e01048ffe9197fde15b972b42ce7c4094e4b4a47
Iago V. Rocha <iagovieira3562@gmail.com>
2025-05-23 17:06:23 -0300

    Atualização na classe Funcionario, cadastrar diretamente ao instaciar o objeto

--- Funcionario.py
+++ Funcionario.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from Pessoa import Pessoa
 class Funcionario(Pessoa):
-    def __init__(self, sistema: "Sistema", nome: str, rg: str, cpf: str, anoNasc: int, mesNasc: int, # type: ignore
+    def __init__(self, sistema: Sistema, nome: str, rg: str, cpf: str, anoNasc: int, mesNasc: int, # type: ignore
                   diaNasc: int, sexo: str, matricula: int, setor: str, cargo: str, nivel: int):
         Pessoa.__init__(self, nome, rg, cpf, anoNasc, mesNasc, diaNasc, sexo)
         self.sistema = sistema
@@ -9,9 +9,8 @@ class Funcionario(Pessoa):
         self.__setor = setor
         self.__cargo = cargo
         self.__nivel = nivel
+        self.sistema.cadastroFuncionario(self)
 
-    # def cadastrarFuncionario(self):
-    #     self.sistema.cadastroFuncionario(self)
     def getMatricula(self) -> str:
         return self.__matricula
     def getSetor(self) -> str:
