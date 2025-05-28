baa3965f7bb6fa1a192d33b124e57cce62bcfb59
Iago V. Rocha <iagovieira3562@gmail.com>
2025-05-28 20:40:26 -0300

    feat: adicionado instanciação de coordenadorAdm para testes

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from __future__ import annotations
 from Funcionario import Funcionario
+from CoordenadorAdm import CoordenadorAdm
 from Sistema import Sistema
 from Salario import *
 
 if __name__ == "__main__":
     sistema = Sistema()
-    funcionario1 = Funcionario(sistema, "NOME FUNCIONARIO", "XX.XXX.XXX-X", "XXX.XXX.XXX-XX", 1993, 11, 11, "Masculino", 1, "Administrativo", "Funcionario Administrativo", 1)
+    funcionario1 = Funcionario(sistema, "NOME FUNCIONARIO", "XX.XXX.XXX-X", "XXX.XXX.XXX-XX", 1993, 11, 11, "Masculino", 1, "Administrativo", "Funcionario Administrativo", "A")
     # print(sistema.cadastroFuncionario(funcionario1))
-    print(sistema.cadastroFuncionario(funcionario1))
+    print(sistema.cadastrarFuncionario(funcionario1))
     print(sistema.getCadastroFuncionarios())
     sistema.getCadastroFuncionarios()[1].Exibir()
 
-    salario = Salario(True, strategy = ProfessorNivelI)
-    print(salario.calcularSalario())
+    salario = Salario(strategy = TecnicoAdministrativoE)
+    print(salario.getSalarioLiquido())
+    coordenadorAdm01 = CoordenadorAdm(sistema, "NOME COORDENADOR", "XX.XXX.XXX-X", "XXX.XXX.XXX-XX", 1993, 11, 11, "Masculino", 1, "Administrativo", "Funcionario Administrativo", "E", "RH")
+    print(sistema.getCoordenadoresAdministrativo()[1].get_nome())
+    print(sistema.getCoordenadoresAdministrativo()[1].calcularPlusSalario())
+    sistema.getCoordenadoresAdministrativo()[1].Exibir()
