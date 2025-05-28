51d0ac7691adbc5efe0e71bd813b9d4059fac0a1
João Paulo <joao.lordelo@souunit.com.br>
2025-05-28 19:56:28 -0300

    Adição dos Getters e import de annotations

--- Curso.py
+++ Curso.py
@@ -1,10 +1,8 @@
-from Professor import Professor
-from Coordenador import Coordenador
-
+from __future__ import annotations
 class Curso:
 
     #Construtor da Classe Curso
-    def __init__(self,titulo :str,descricao :str,sala :str,idCurso :int, professor :Professor, coordenador :Coordenador):
+    def __init__(self,titulo :str,descricao :str,sala :str,idCurso :int, professor :Professor, coordenador :Coordenador): # type: ignore
         self.__titulo = titulo
         self.__descricao = descricao
         self.__valor = 865.23
@@ -14,23 +12,34 @@ class Curso:
         self.coordenador = coordenador 
         self.numMinAluno = None
 
+    def getTitulo(self) -> str:
+        return self.__titulo
+    
+    def getDescricao(self) -> str:
+        return self.__descricao
+    
+    def getValor(self) -> float:
+        return self.__valor
+    
+    def getId(self) -> int:
+        return self.__idCurso
+
     def cadastrarCurso():
         #Vai ficar faltando
         return 
     
     def exibirCurso(self) -> str:
         return f"""
-        Curso: {self.__titulo}
-        Descrição: {self.__descricao}
-        Custo: {self.__valor}
+        Curso: {self.getTitulo()}
+        Descrição: {self.getDescricao()}
+        Custo: {self.getValor()}
         Sala: {self.sala}
-        ID: {self.__idCurso}
+        ID: {self.getId()}
         Professor: {self.professor}
         Coordenador: {self.coordenador}
         Numero Minimo de Alunos: {self.numMinAluno}
-
         """
     
     def calcularNumMinAluno(self) -> int:
-        self.numMinAluno = self.professor.getSalario()/self.__valor
+        self.numMinAluno = self.professor.getSalario() / self.__valor
         return int(f"{self.numMinAluno:.0f}")
