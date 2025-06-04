58dfa888f4d570bcff4e3a73110f5e30da806544
Iago V. Rocha <iagovieira3562@gmail.com>
2025-06-04 02:38:06 -0300

    style: forma como o é motrado as informações

--- Professor.py
+++ Professor.py
@@ -2,11 +2,12 @@ from __future__ import annotations
 from Funcionario import Funcionario
 
 class Professor(Funcionario):
-    def __init__(self, sistema, nome, rg, cpf, anoNasc, mesNasc, diaNasc, sexo, matricula, setor, cargo, nivel, formacao, disciplina):
+    def __init__(self, sistema: Sistema, nome: str, rg: str, cpf: str, anoNasc: int, mesNasc: int, # type: ignore
+                  diaNasc: int, sexo: str, matricula: int, setor: str, cargo: str, nivel: str, formacao: str, disciplina: str):
         Funcionario.__init__(self, sistema, nome, rg, cpf, anoNasc, mesNasc, diaNasc, sexo, matricula, setor, cargo, nivel)
         self.__formacao = formacao
         self.__disciplina  = disciplina
-        
+        sistema.cadastrarProfessor(self)
 
     def getFormacao(self) -> str:
         return self.__formacao
@@ -14,8 +15,16 @@ class Professor(Funcionario):
     def getDisciplina(self) -> str:
         return self.__disciplina
 
+    def setDisciplina(self, disciplina):
+        self.__disciplina = disciplina
+
+    def setFormacao(self, formacao):
+        self.__formacao = formacao
+
     def Exibir(self):
         Funcionario.Exibir(self)
         print(f"{' Informações Professor ':-^50}")
-        print(f"Formação: {self.getFormacao()}")  
-        print(f"Disciplina: {self.getDisciplina()}")
+        print(f"""
+            Formação: {self.getFormacao()}
+            Disciplina: {self.getDisciplina()}
+        """)
