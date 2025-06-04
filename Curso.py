6b30df34105b4bcf261281cb6b50e4b1d593c1a0
Iago V. Rocha <iagovieira3562@gmail.com>
2025-06-04 02:47:08 -0300

    style: forma como é mostrada as informações de Curso

--- Curso.py
+++ Curso.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 class Curso:
 
     #Construtor da Classe Curso
-    def __init__(self,titulo :str,descricao :str,sala :str,idCurso :int, professor :Professor, coordenador :Coordenador): # type: ignore
+    def __init__(self, titulo: str, descricao: str, sala: str, idCurso: int, professor: Professor, coordenador: Coordenador): # type: ignore
         self.__titulo = titulo
         self.__descricao = descricao
         self.__valor = 865.23
@@ -29,16 +29,17 @@ class Curso:
         return 
     
     def Exibir(self) -> str:
-        return f"""
-        Curso: {self.getTitulo()}
-        Descrição: {self.getDescricao()}
-        Custo: {self.getValor()}
-        Sala: {self.sala}
-        ID: {self.getId()}
-        Professor: {self.professor}
-        Coordenador: {self.coordenador}
-        Numero Minimo de Alunos: {self.numMinAluno}
-        """
+        print(f"{' Informações Curso ':-^50}")
+        print(f"""
+            Curso: {self.getTitulo()}
+            Descrição: {self.getDescricao()}
+            Custo: {self.getValor()}
+            Sala: {self.sala}
+            ID: {self.getId()}
+            Professor: {self.professor}
+            Coordenador: {self.coordenador}
+            Numero Minimo de Alunos: {self.numMinAluno}
+        """)
     
     def calcularNumMinAluno(self) -> int:
         self.numMinAluno = self.professor.getSalario() / self.__valor
