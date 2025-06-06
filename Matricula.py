beb947b93156bb28fce536579a74a32cc119547e
João Paulo <joao.lordelo@souunit.com.br>
2025-06-05 23:05:56 -0300

    Finalização de matricula

--- Matricula.py
+++ Matricula.py
@@ -1,21 +1,11 @@
 from __future__ import annotations
 import datetime
-from Aluno import Aluno
-from Curso import Curso
 
 
 class Matricula:
-    def __init__(self, sistema: Sistema, aluno: Aluno, curso: Curso):  # type: ignore
-        self.__aluno = aluno
-        self.__curso = curso
+    def __init__(self, sistema: Sistema):  # type: ignore
         self.__dataMatricula = datetime.date.today()
         self.__idMatricula = len(sistema.getCadastroAlunos()) + 1
-
-    def getAluno(self) -> Aluno: 
-        return self.__aluno
-
-    def getCurso(self) -> Curso: 
-        return self.__curso
 
     def getDataMatricula(self) -> datetime.date:
         return self.__dataMatricula
@@ -30,7 +20,5 @@ class Matricula:
         print(f"{' Informações da Matrícula ':-^50}")
         print(f"""
             ID da Matrícula: {self.getIdMatricula()}
-            Aluno: {self.getAluno().getNome()} (Código: {self.getAluno().getCodigo()})
-            Curso: {self.getCurso().getTitulo()} (ID: {self.getCurso().getId()})
             Data da Matrícula: {self.getDataMatricula().strftime('%d/%m/%Y')}
             """)
